38529cda1e791082e0bc2fbc26a84cd8acf33e83
Ryan Gaus <rsg1egoman@gmail.com>
2018-09-24 08:27:47 -0400

    code: add favicon / branding stuff

--- components/head.js
+++ components/head.js
@@ -17,11 +17,17 @@ const Head = props => (
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-      <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
-      <link rel="apple-touch-icon" href="/static/touch-icon.png" />
-      <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
-      <link rel="icon" href="/static/favicon.ico" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
+      <link rel="manifest" href="/static/site.webmanifest" />
+      <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="msapplication-config" content="/static/browserconfig.xml" />
+      <meta name="theme-color" content="#ffffff" />
 
       <meta property="og:url" content={props.url || defaultOGURL} />
       <meta property="og:title" content={props.title || ''} />
